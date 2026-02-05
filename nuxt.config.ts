acedf28a31aa90a20ee74a00a725573864480e7b
yuki0829 <fledgling0829@gmail.com>
2026-02-05 14:50:31 +0900

    chore: CSS読み込み設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-07-15',
 })
