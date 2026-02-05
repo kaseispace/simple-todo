d8c2b120dbb7b4506bd98292ad48c6a6cb9d46b6
yuki0829 <fledgling0829@gmail.com>
2026-02-05 15:10:46 +0900

    chore: ESLintのstylisticルールを有効化する設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
