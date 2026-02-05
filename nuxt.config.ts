b9c71c7eb1e46df197a6601c0702195e54eb5312
yuki0829 <fledgling0829@gmail.com>
2026-02-05 20:48:21 +0900

    chore: #schemasエイリアスを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { fileURLToPath } from 'node:url'
+
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/ui'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  alias: {
+    '#schemas': fileURLToPath(new URL('./schemas', import.meta.url)),
+  },
   compatibilityDate: '2025-07-15',
   eslint: {
     config: {
