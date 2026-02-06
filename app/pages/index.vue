a65c0b571e983a77031b16ef4caf6ae34696a97a
yuki0829 <fledgling0829@gmail.com>
2026-02-06 21:14:14 +0900

    feat: useCategoryListを利用したカテゴリ一覧ページを実装

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,2 +1,34 @@
 <script setup lang="ts">
+const { categoryList } = useCategoryList()
 </script>
+
+<template>
+  <UPageGrid>
+    <UCard
+      v-for="category in categoryList"
+      :key="category.id"
+    >
+      <h3 class="text-lg font-semibold flex items-center justify-center gap-1">
+        <UIcon
+          name="i-lucide-tag"
+          class="size-5 bg-gray-500"
+        />
+        {{ category.title }}
+      </h3>
+
+      <ul v-if="category.notes.length > 0">
+        <li
+          v-for="note in category.notes"
+          :key="note.id"
+          class="flex items-center gap-2 border-b pb-1"
+        >
+          <span>□ {{ note.content }}</span>
+        </li>
+      </ul>
+
+      <p v-else>
+        タスクはありません
+      </p>
+    </UCard>
+  </UPageGrid>
+</template>
