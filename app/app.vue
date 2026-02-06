3db57a461ada32e18a7e3601b0e408a9e4e1d879
yuki0829 <fledgling0829@gmail.com>
2026-02-06 21:11:42 +0900

    feat: 共通ヘッダーを追加

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,15 @@
 <template>
   <UApp>
+    <UHeader>
+      <template #title>
+        ToDo リスト
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+
     <NuxtPage />
   </UApp>
 </template>
